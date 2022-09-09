c1e09956b5054086d29fb08239e5792c625e58da
Anderson <andersonmurdog@gmail.com>
2022-09-09 09:48:40 -0500

    manejo de funciones computed que son las q se guardan en cache

---
+++ src/components/Counter.vue
@@ -0,0 +1,79 @@
+<template>
+  <h2>{{ custumTitle }}</h2>
+  <p> {{ counter}} <sub>2</sub> = {{ squereCounter }}</p>
+
+  <div>
+    <button @click="increase">
+      +1
+    </button>
+    <button v-on:click="decrease">
+      -1
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+
+  props: ['title'],
+    name:'Counter',
+    // se crea dentro de la data xq de esta manera todo el obj lo q este dentro sera reactivo (que cualquier cambio que sufra va  areaccionar acorde)
+    data() {
+      return {
+        counter: 5
+      }
+    },
+    methods: {
+      getSquereValue(){
+        return this.counter * this.counter
+      },
+      increase(){
+        this.counter +=1 ;
+
+      },
+
+      decrease(){
+        this.counter -=1 ;
+
+
+      }
+    },
+    // estos metodos se guardan en cache haciendo mas 
+    // eficiente el proceso todo metodod e estos debe tener un return
+    computed: {
+      //todo co
+      squereCounter(){
+        console.log('Metodo computado');
+        return this.counter * this.counter
+      },
+      custumTitle(){
+        console.log('title', this.title.length);
+
+        return this.title.length ?  this.title: 'Counter';
+        
+
+      }
+
+      
+    }
+
+}
+</script>
+
+<style>
+  button{
+    background-color: #64BB87;
+    border-radius: 5px;
+    color: white;
+    margin: 0 5px;
+    padding: 5px 15px;
+    transition: 0.3s ease-in-out;
+    
+  }
+
+  button:hover{
+    background-color: #5aa67b;
+    transition: 0.3s ease-in-out;
+
+  }
+</style>
